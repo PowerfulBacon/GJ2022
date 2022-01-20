e1574470f42afe463a5607a95b43dde396306541
PowerfulBacon <>
2022-01-20 10:13:11 +0000

    a

---
+++ GJ2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GJ2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
