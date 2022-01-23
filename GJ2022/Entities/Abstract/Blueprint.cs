aea709c43600052d1a610ddc8f21ce8d93da9d74
PowerfulBacon <>
2022-01-23 11:21:43 +0000

    Adds in build mode subsystem + mouse hook trackers

---
+++ GJ2022/Entities/Abstract/Blueprint.cs
@@ -0,0 +1,33 @@
+﻿using GJ2022.Entities.ComponentInterfaces;
+using GJ2022.Utility.MathConstructs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJ2022.Entities.Abstract
+{
+    public class Blueprint : Entity, IDestroyable
+    {
+
+        private bool isDestroyed = false;
+
+        public Blueprint(Vector position) : base(position)
+        { }
+
+        public bool Destroy()
+        {
+            //Set destroyed
+            isDestroyed = true;
+            //Stop rendering
+            
+            return true;
+        }
+
+        public bool IsDestroyed()
+        {
+            return isDestroyed;
+        }
+    }
+}
---
+++ GJ2022/Subsystems/BuildModeSubsystem.cs
@@ -0,0 +1,68 @@
+﻿using GJ2022.GlobalDataComponents;
+using GJ2022.Utility.MathConstructs;
+using GLFW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJ2022.Subsystems
+{
+    class BuildModeSubsystem : Subsystem
+    {
+
+        public static BuildModeSubsystem Singleton { get; } = new BuildModeSubsystem();
+
+        public override int sleepDelay => 50;
+
+        public override SubsystemFlags SubsystemFlags => SubsystemFlags.NO_PROCESSING;
+
+        private bool buildModeActive = false;
+        private bool isDragging = false;
+
+        private Vector dragStartPoint;
+        private Vector dragEndPoint;
+
+        private List<Todo> dragHighlights = new List<Todo>();
+
+        public override void Fire(Window window)
+        {
+            //Don't fire if build mode isn't active
+            if (!buildModeActive)
+                return;
+            //Check if dragging
+            if (!isDragging)
+            {
+                //Mouse button isn't down.
+                if (Glfw.GetMouseButton(window, MouseButton.Left) != InputState.Press)
+                    return;
+                //Mouse button is down, start dragging
+                isDragging = true;
+                //Mark the location of where we started dragging from
+            }
+        }
+
+        public override void InitSystem()
+        { }
+
+        protected override void AfterWorldInit()
+        { }
+
+        private void ClearDragHighlights()
+        {
+
+        }
+
+        public void ActivateBuildMode()
+        {
+            buildModeActive = true;
+        }
+
+        public void DisableBuildMode()
+        {
+            buildModeActive = false;
+        }
+
+    }
+}
