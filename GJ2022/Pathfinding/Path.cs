ce5fac657ee90a1e685e701e8bb8e6f458085ae2
PowerfulBacon <>
2022-01-23 18:15:00 +0000

    path basics

---
+++ GJ2022/Pathfinding/Path.cs
@@ -0,0 +1,17 @@
+﻿using GJ2022.Utility.MathConstructs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJ2022.Pathfinding
+{
+    public class Path
+    {
+
+        //List of points this path covers
+        public List<Vector> points;
+
+    }
+}
---
+++ GJ2022/Subsystems/PathfindingSystem.cs
@@ -0,0 +1,37 @@
+﻿using GJ2022.Utility.MathConstructs;
+using GLFW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJ2022.Subsystems
+{
+    public class PathfindingSystem : Subsystem
+    {
+
+        //20 fps :)
+        public override int sleepDelay => 50;
+
+        //No processing, but fires
+        public override SubsystemFlags SubsystemFlags => SubsystemFlags.NO_PROCESSING;
+
+        public override void Fire(Window window)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InitSystem()
+        { }
+
+        protected override void AfterWorldInit()
+        { }
+
+        private void ProcessPath(Vector start, Vector end)
+        {
+
+        }
+
+    }
+}
