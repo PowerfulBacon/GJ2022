efead7d536c64f23c5da5e4b8771dabc199d40e8
PowerfulBacon <>
2022-01-25 12:34:47 +0000

    Create UserInterfaceComponent.cs

---
+++ GJ2022/UserInterface/Components/UserInterfaceComponent.cs
@@ -0,0 +1,19 @@
+﻿using GJ2022.Rendering.RenderSystems.Renderables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJ2022.UserInterface.Components
+{
+    public abstract class UserInterfaceComponent
+    {
+
+        //The renderable attached to thie UI component.
+        public abstract Renderable Renderable { get; }
+
+        //Positional stuff
+
+    }
+}
