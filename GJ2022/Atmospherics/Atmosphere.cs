94556c44510d866d0476a23c1543abc83fb6602e
PowerfulBacon <>
2022-01-26 19:01:03 +0000

    Atmospheric basics

---
+++ GJ2022/Atmospherics/Atmosphere.cs
@@ -0,0 +1,31 @@
+﻿using GJ2022.Atmospherics.Gasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJ2022.Atmospherics
+{
+    public class Atmosphere
+    {
+
+        public static Atmosphere SpaceAtmosphere = new Atmosphere();
+        public static Atmosphere IdealAtmosphere = new Atmosphere(new PressurisedGas(Nitrogen.Singleton, ));
+
+        //The atmospheric contents of this atmosphere
+        public Dictionary<Gas, PressurisedGas> AtmosphericContents { get; } = new Dictionary<Gas, PressurisedGas>();
+
+        //Temperature of this atmosphere
+        public float KelvinTemperature { get; }
+
+        public Atmosphere(params PressurisedGas[] gasses)
+        {
+            foreach (PressurisedGas gas in gasses)
+            {
+                AtmosphericContents.Add(gas.gas, gas);
+            }
+        }
+
+    }
+}
---
+++ GJ2022/Atmospherics/AtmosphericConstants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJ2022.Atmospherics
+{
+    public static class AtmosphericConstants
+    {
+
+        //Ideal pressure
+        public const float IDEAL_PRESSURE = 101.33f;
+        //Ideal temperature of gasses
+        public const float IDEAL_TEMPERATURE = 295.15f;
+        //How many liters of gas in a single tile (1 cubic meter)
+        public const float TILE_GAS_VOLUME = 1000;
+
+        private const float IDEAL_GAS_CONSTANT = (IDEAL_PRESSURE * TILE_GAS_VOLUME) / (41.27f * IDEAL_TEMPERATURE)
+
+        public static float CalculateMoles(float pressure, float volume, float temperature)
+        {
+            return (pressure * volume) / (IDEAL_GAS_CONSTANT * temperature);
+        }
+
+    }
+}
