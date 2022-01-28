37423497b3ca7c3958d5874b39fb19c3cba1a063
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-01-28 18:16:14 +0000

    Gold buildings

---
+++ GJ2022/Entities/Turfs/Standard/Solids/GoldWall.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJ2022.Entities.Turfs.Standard.Solids
+{
+    public class GoldWall : Solid
+    {
+
+        protected override string Texture { get; } = "gold_wall";
+
+        public GoldWall(int x, int y) : base(x, y)
+        {
+        }
+
+    }
+}
