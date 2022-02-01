d3e5eff53968fd6168faf383e85f16a8d1a7f58c
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-02-01 18:21:53 +0000

    Update Limb.cs

--- GJ2022/Entities/Pawns/Health/Bodyparts/Limbs/Limb.cs
+++ GJ2022/Entities/Pawns/Health/Bodyparts/Limbs/Limb.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GJ2022.Utility.MathConstructs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,17 @@ namespace GJ2022.Entities.Pawns.Health.Bodyparts.Limbs
     abstract class Limb : Bodypart
     {
 
+        //An array containing all the allowed slots this limb can go in.
+        //(Legs can go into leg slots etc.)
+        public abstract string[] allowedSlots { get; }
 
+        //The name of the texture that gets applied to humans if this limb is applied to them.
+        //Ignore if this can't be applied to humans
+        public virtual string HumanOverlayTextureId { get; } = null;
+
+        //The colour of the limb when applied to a human.
+        //(A corgi leg applied to a human looks like a human leg but with corgi fur colour)
+        public virtual Colour? HumanOverlayColour { get; } = null;
 
     }
 
