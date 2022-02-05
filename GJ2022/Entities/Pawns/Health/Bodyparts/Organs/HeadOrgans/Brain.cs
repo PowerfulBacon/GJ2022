7c7eb231d99461af9fbb443efb12e38e474de6fe
PowerfulBacon <>
2022-02-05 22:08:40 +0000

    Removed the writeline message

--- GJ2022/Entities/Pawns/Health/Bodyparts/Organs/HeadOrgans/Brain.cs
+++ GJ2022/Entities/Pawns/Health/Bodyparts/Organs/HeadOrgans/Brain.cs
@@ -46,8 +46,6 @@ namespace GJ2022.Entities.Pawns.Health.Bodyparts.Organs.HeadOrgans
             {
                 float damageProportion = 1 - (availableOxygen / OxygenConsumptionRate * deltaTime * OxygenDamageProportion);
                 AddInjury(new Hypoxia(damageProportion * OxygenDamageMultiplied * deltaTime));
-                //TODO: Apply damage
-                Log.WriteLine($"A brain just took {damageProportion * OxygenDamageMultiplied * deltaTime} damage! New health: {Health}");
             }
         }
 
