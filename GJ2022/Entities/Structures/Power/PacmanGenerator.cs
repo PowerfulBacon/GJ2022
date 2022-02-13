47033804b72379c1a494587d6bc3f81279d8a3ea
Jake Mortlock (jm15g21) <jm15g21@soton.ac.uk>
2022-02-13 21:28:40 +0000

    Update PacmanGenerator.cs

--- GJ2022/Entities/Structures/Power/PacmanGenerator.cs
+++ GJ2022/Entities/Structures/Power/PacmanGenerator.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GJ2022.Rendering.RenderSystems.Renderables;
+using GJ2022.Utility.MathConstructs;
 
 namespace GJ2022.Entities.Structures.Power
 {
-    class PacmanGenerator
+    class PacmanGenerator : Structure
     {
+
+        public PacmanGenerator(Vector<float> position, float layer) : base(position, layer)
+        { }
+
+        public override Renderable Renderable { get; set; } = new StandardRenderable("power.portgen0_0");
+
     }
 }
