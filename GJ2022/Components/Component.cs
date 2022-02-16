8d2e44f6a210b31e72d7408cf7d51371527b0ae7
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-02-16 17:06:02 +0000

    XML File examples

---
+++ GJ2022/Components/Component.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJ2022.Components
+{
+    public abstract class Component
+    {
+
+        public abstract void OnComponentAdd();
+
+        public abstract void OnComponentRemove();
+
+    }
+}
