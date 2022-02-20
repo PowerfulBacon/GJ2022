038526053f8f9a0e61950828393bb02d26c79cbd
PowerfulBacon <>
2022-02-20 00:29:09 +0000

    Changes the component equippable namespace

--- GJ2022/Components/Items/Component_Equippable.cs
+++ GJ2022/Components/Items/Component_Equippable.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GJ2022.Components.Atmospherics
+namespace GJ2022.Components.Items
 {
     public class Component_Equippable : Component
     {
