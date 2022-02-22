c936ea9e35401932515a1a0032d61006ba2cc575
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-02-22 18:41:42 +0000

    Create Component_InternalEntity.cs

---
+++ GJ2022/Components/Items/Component_InternalEntity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJ2022.Components.Items
+{
+    public class Component_InternalEntity : Component
+    {
+
+        public string StoredEntityName { get; set; }
+
+        public Entity StoredEntity { get; set; }
+
+        public override void OnComponentAdd()
+        {
+            //Create the stored entity
+
+            throw new NotImplementedException();
+        }
+
+        public override void OnComponentRemove()
+        {
+            //Destroy the stored entity
+            throw new NotImplementedException();
+        }
+
+        public override void SetProperty(string name, object property)
+        {
+            switch (name)
+            {
+                case "StoredEntityName":
+                    StoredEntityName = (string)property;
+                    return;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
